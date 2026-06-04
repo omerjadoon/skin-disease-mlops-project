24efee15bbdc8f4e5fdd5bfd1b89bfa2c8e945bf
Omer Khan Jadoon <omerjadoon1@gmail.com>
2026-06-04 22:29:43 +0200

    fixed container issues

--- scripts/postgres-init.sh
+++ scripts/postgres-init.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 function create_database() {
     local database=$1
     echo "Creating database: $database"
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
         SELECT 'CREATE DATABASE $database'
         WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$database')\gexec
 EOSQL
